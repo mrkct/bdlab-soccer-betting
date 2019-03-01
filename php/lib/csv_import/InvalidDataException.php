b1d2246a4de1e6211adcfef85cdb8fba7e083329
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-03-01 23:50:52 +0100

    Removed mention of country table in comment

--- php/lib/csv_import/InvalidDataException.php
+++ php/lib/csv_import/InvalidDataException.php
@@ -2,7 +2,6 @@
 /**
  * An exception to be thrown when, while inserting a
  * csv row in the db, the row cannot be inserted
- * because of errors impossible to solve. For example,
- * an invalid country name
+ * because of errors impossible to solve. 
  */
 class InvalidDataException extends Exception {}
