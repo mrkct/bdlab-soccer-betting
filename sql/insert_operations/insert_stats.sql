eac9854f8123d5f99ead34bbe4b53a4c9df57ef1
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-03-05 17:31:24 +0100

    created pgsql functions for adding stats and teams

---
+++ sql/insert_operations/insert_stats.sql
@@ -0,0 +1,170 @@
+/**
+ * Inserts a player stats data and returns an integer representing the status
+ * of the operation. Status codes are:
+ * 0    : OK
+ * -1   : User is not allowed to insert
+ * -2   : Duplicate row
+ * -3   : Foreign key violation
+ * When an error occurs outside the ones specified above an exception is thrown
+ */
+CREATE OR REPLACE FUNCTION insert_team(
+    collaborator_id INTEGER, 
+    player INTEGER,
+    attribute_date DATE,
+    overall_rating INTEGER,
+    potential INTEGER,
+    preferred_foot VARCHAR(5),
+    attacking_work_rate VARCHAR(6),
+    defensive_work_rate VARCHAR(6),
+    crossing INTEGER,
+    finishing INTEGER,
+    heading_accuracy INTEGER,
+    short_passing INTEGER,
+    volleys INTEGER,
+    dribbling INTEGER,
+    curve INTEGER,
+    free_kick_accuracy INTEGER,
+    long_passing INTEGER,
+    ball_control INTEGER,
+    acceleration INTEGER,
+    sprint_speed INTEGER,
+    agility INTEGER,
+    reactions INTEGER,
+    balance INTEGER,
+    shot_power INTEGER,
+    jumping INTEGER,
+    stamina INTEGER,
+    strength INTEGER,
+    long_shots INTEGER,
+    aggression INTEGER,
+    interceptions INTEGER,
+    positioning INTEGER,
+    vision INTEGER,
+    penalties INTEGER,
+    marking INTEGER,
+    standing_tackle INTEGER,
+    sliding_tackle INTEGER,
+    gk_diving INTEGER,
+    gk_handling INTEGER,
+    gk_kicking INTEGER,
+    gk_positioning INTEGER,
+    gk_reflexes INTEGER
+) RETURNS QueryResult AS $$
+DECLARE
+    current_collaborator soccer.collaborator%ROWTYPE;
+    result QueryResult;
+BEGIN
+    SELECT * INTO current_collaborator 
+    FROM collaborator 
+    WHERE id = collaborator_id;
+
+    IF current_collaborator.role <> 'administrator' THEN
+        result.success := FALSE;
+        result.error_code := -1;
+        result.message := 'User is not allowed to insert stats';
+        RETURN result;
+    END IF;
+
+    INSERT INTO stats(
+        player,
+        attribute_date,
+        overall_rating,
+        potential,
+        preferred_foot,
+        attacking_work_rate,
+        defensive_work_rate,
+        crossing,
+        finishing,
+        heading_accuracy,
+        short_passing,
+        volleys,
+        dribbling,
+        curve,
+        free_kick_accuracy,
+        long_passing,
+        ball_control,
+        acceleration,
+        sprint_speed,
+        agility,
+        reactions,
+        balance,
+        shot_power,
+        jumping,
+        stamina,
+        strength,
+        long_shots,
+        aggression,
+        interceptions,
+        positioning,
+        vision,
+        penalties,
+        marking,
+        standing_tackle,
+        sliding_tackle,
+        gk_diving,
+        gk_handling,
+        gk_kicking,
+        gk_positioning,
+        gk_reflexes
+    ) VALUES (
+        player_id,
+        attribute_date,
+        overall_rating,
+        potential,
+        preferred_foot,
+        attacking_work_rate,
+        defensive_work_rate,
+        crossing,
+        finishing,
+        heading_accuracy,
+        short_passing,
+        volleys,
+        dribbling,
+        curve,
+        free_kick_accuracy,
+        long_passing,
+        ball_control,
+        acceleration,
+        sprint_speed,
+        agility,
+        reactions,
+        balance,
+        shot_power,
+        jumping,
+        stamina,
+        strength,
+        long_shots,
+        aggression,
+        interceptions,
+        positioning,
+        vision,
+        penalties,
+        marking,
+        standing_tackle,
+        sliding_tackle,
+        gk_diving,
+        gk_handling,
+        gk_kicking,
+        gk_positioning,
+        gk_reflexes
+    );
+    
+    result.success := TRUE;
+    result.error_code := 0;
+    result.message := NULL;
+    
+    RETURN result;
+
+    EXCEPTION
+        WHEN unique_violation THEN
+            result.success := FALSE;
+            result.error_code := -2;
+            result.message := 'A row with the same primary key already exists';
+            RETURN result;
+        WHEN foreign_key_violation THEN
+            result.success := FALSE;
+            result.error_code := -3;
+            result.message := 'A foreign key violation occurred';
+            RETURN result;
+END;
+$$ language 'plpgsql';
---
+++ sql/insert_operations/insert_team.sql
@@ -0,0 +1,51 @@
+/**
+ * Inserts a team data and returns an integer representing the status
+ * of the operation. Status codes are:
+ * 0    : OK
+ * -1   : User is not allowed to insert
+ * -2   : Duplicate row
+ * -3   : Foreign key violation
+ * When an error occurs outside the ones specified above an exception is thrown
+ */
+CREATE OR REPLACE FUNCTION insert_team(
+    collaborator_id INTEGER, 
+    team_id INTEGER, 
+    shortname VARCHAR(100),
+    longname VARCHAR(255)
+) RETURNS QueryResult AS $$
+DECLARE
+    current_collaborator soccer.collaborator%ROWTYPE;
+    result QueryResult;
+BEGIN
+    SELECT * INTO current_collaborator 
+    FROM collaborator 
+    WHERE id = collaborator_id;
+
+    IF current_collaborator.role <> 'administrator' THEN
+        result.success := FALSE;
+        result.error_code := -1;
+        result.message := 'User is not allowed to insert teams';
+        RETURN result;
+    END IF;
+
+    INSERT INTO team(id, shortname, longname) VALUES (team_id, shortname, longname);
+    
+    result.success := TRUE;
+    result.error_code := 0;
+    result.message := NULL;
+    
+    RETURN result;
+
+    EXCEPTION
+        WHEN unique_violation THEN
+            result.success := FALSE;
+            result.error_code := -2;
+            result.message := 'A row with the same primary key already exists';
+            RETURN result;
+        WHEN foreign_key_violation THEN
+            result.success := FALSE;
+            result.error_code := -3;
+            result.message := 'A foreign key violation occurred';
+            RETURN result;
+END;
+$$ language 'plpgsql';
