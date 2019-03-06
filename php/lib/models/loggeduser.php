47b503a50ab9982580d3a127897aa4373e974763
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-03-06 19:19:50 +0100

    added as a wrapper for the session superglobal

---
+++ php/lib/models/loggeduser.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * A helper class for the data in the $_SESSION superglobal
+ * for an already logged in user
+ */
+class LoggedUser{
+    /**
+     * Starts the session if it wasn't already started
+     */
+    private static function sessionPrepare(){
+        if( session_status() == PHP_SESSION_NONE ){
+            session_start();
+        }
+    }
+
+    /**
+     * Returns the argument key in the $_SESSION superglobal
+     * if it is set, returns NULL otherwise
+     */
+    private static function getKey($key){
+        if( isset($_SESSION[$key]) ){
+            return $_SESSION[$key];
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Returns the logged user id or NULL if the
+     * user is not logged
+     */
+    public static function getId(){
+        LoggedUser::sessionPrepare();
+        return LoggedUser::getKey('id');
+    }
+
+    /**
+     * Returns the logged user's name or NULL if the
+     * user is not logged
+     */
+    public static function getName(){
+        sessionPrepare();
+        return getKey('name');
+    }
+
+    /**
+     * Returns the logged user's role of NULL if the
+     * user is not logged
+     */
+    public static function getRole(){
+        LoggedUser::sessionPrepare();
+        return getKey('role');
+    }
+}
