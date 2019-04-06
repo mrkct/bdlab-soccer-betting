9a778d71515f99dd617353423609a596f462b898
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-04-06 14:24:54 +0200

    now uses create_paginated_select

--- php/pages/teams.php
+++ php/pages/teams.php
@@ -1,29 +1,8 @@
-<?php require_once('config.php'); ?>
-<?php
+<?php    
+    require_once('config.php');
     require_once(LIB . '/database.php');
+    require_once(COMPONENTS . '/paginated-select.php');
     $db = db_connect();
-    $pagesize = 10;
-    $page = 1;
-    $offset = 0;
-    if( isset($_GET["pagesize"]) ){
-        $pagesize = min(25, intval($_GET["pagesize"]));
-    }
-    if( isset($_GET["page"]) ){
-        $page = max(1, intval($_GET["page"]));
-    }
-
-    $count_result = pg_query($db, 'SELECT COUNT(*) as total_teams FROM team;');
-    $total_teams = pg_fetch_row($count_result)[0];
-    $total_pages = ceil($total_teams / $pagesize);
-    
-    $page = max(1, min($page, $total_pages));
-    $offset = ($page-1) * $pagesize;
-    pg_prepare(
-        $db, 
-        'get_teams', 
-        'SELECT * FROM team ORDER BY shortname LIMIT $1 OFFSET $2'
-    );
-    $result = pg_execute($db, 'get_teams', array($pagesize, $offset));
 ?>
 <!DOCTYPE html5>
 <html>
@@ -38,20 +17,20 @@
             <h1 class="title is-1 title-centered">
                 All registered teams
             </h1>
-            <div class="list is-hoverable">
-                <?php
-                    while($team = pg_fetch_assoc($result) ):
-                        ?>
-                        <a href="view/team.php?id=<?php echo $team["id"]; ?>" class="list-item">
-                            <?php echo $team["shortname"], ": ", $team["longname"]; ?>
-                        </a>
-                <?php
-                    endwhile;
-                ?>
-            </div>
             <?php
-                require_once(COMPONENTS . '/pagination.php');
-                create_pagination($page, $total_pages, "?page=%d&pagesize=" . $pagesize);
+                $total_teams = pg_fetch_assoc(pg_query("SELECT COUNT(*) AS teams FROM team;"))["teams"];
+                $display_team = function($item){
+                    return sprintf("%s: %s", $item["shortname"], $item["longname"]);
+                };
+                $link_team = function($item){
+                    return sprintf("view/team.php?id=%d", $item["id"]);
+                };
+                create_paginated_select_form(
+                    "SELECT * FROM team ORDER BY shortname LIMIT $1 OFFSET $2", 
+                    $total_teams, 
+                    $display_team, 
+                    $link_team
+                );
             ?>
         </div>
     </body>
