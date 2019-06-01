777eb677020faa06ba8ac7882d2a319be520c0f5
Marco Cutecchia <cutecchia.marco@outlook.it>
2019-06-01 23:25:48 +0200

    edited run script to show httpd messages

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-
 REM Sets environment variables and runs apache
+REM Insert your data here
 
 SET DB_HOST=localhost
 SET DB_NAME=bdlab
@@ -8,6 +8,8 @@ SET DB_SCHEMA=soccer
 SET DB_USER=postgres
 SET DB_PASSWORD=segreto
 SET DB_PORT=5432
+
+@echo on
 
 echo Running with db %DB_HOST%:%DB_PORT% on %DB_NAME%.%DB_SCHEMA% with user %DB_USER%
 httpd
