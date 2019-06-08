cc82c0840579c26408e79ad53126712e16123b42
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-06-08 22:40:55 +0200

    added button to view match infos

--- php/pages/view/team.php
+++ php/pages/view/team.php
@@ -69,18 +69,22 @@
                         <th>Home Team</th>
                         <th>Away Team</th>
                         <th>Final Result</th>
+                        <th>More Details</th>
                     </thead>
                     <tbody>
                         <?php
                             while($match = pg_fetch_assoc($result)):
                         ?>
                             <a href="<?php echo PAGES; ?>/view/match.php?id=<?php echo $match["id"]; ?>">
-                            <tr>
-                                <td><?php echo date("d/m/Y", strtotime($match["played_on"])); ?></td>
-                                <td><?php echo $match["hometeam_longname"]; ?></td>
-                                <td><?php echo $match["awayteam_longname"]; ?></td>
-                                <td><?php echo $match["hometeam_goals"], "-", $match["awayteam_goals"]; ?></td>
-                            </tr>
+                                <tr>
+                                    <td><?php echo date("d/m/Y", strtotime($match["played_on"])); ?></td>
+                                    <td><?php echo $match["hometeam_longname"]; ?></td>
+                                    <td><?php echo $match["awayteam_longname"]; ?></td>
+                                    <td><?php echo $match["hometeam_goals"], "-", $match["awayteam_goals"]; ?></td>
+                                    <td>
+                                        <a class="button is-small is-fullwidth" href="<?php echo sprintf("%s/view/match.php?id=%d", PAGES, $match["id"]);?>">View match</a>
+                                    </td>
+                                </tr>
                             </a>
                         <?php endwhile; ?>
                     </tbody>
