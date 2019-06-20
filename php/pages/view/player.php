c8b9094f76cefa54c12361f425f904f0caf42f86
Marco Cutecchia <marco.cutecchia@outlook.it>
2019-06-20 08:41:15 +0200

    recent played matches on view player

--- php/pages/view/player.php
+++ php/pages/view/player.php
@@ -53,6 +53,51 @@
                             <p><b>Birthday: </b><?php echo date('d/m/y', strtotime($player["birthday"])); ?></p>
                         </div>
                     </div>
+                    <h2 class="title is-2 title-centered">
+                        Matches where he played recently
+                    </h2>
+                    <?php
+                        pg_prepare(
+                            $db, 
+                            'recent_matches', 
+                            'SELECT 
+                                match.*,
+                                home.shortname AS hometeam_shortname,
+                                home.longname AS hometeam_longname, 
+                                away.shortname AS awayteam_shortname, 
+                                away.longname AS awayteam_longname
+                             FROM played 
+                             JOIN match ON match.id = played.match
+                             JOIN team AS home ON home.id = match.hometeam
+                             JOIN team AS away ON away.id = match.awayteam 
+                             WHERE player=$1 ORDER BY played_on DESC LIMIT 10;'
+                        );
+                        $result = pg_execute($db, 'recent_matches', array($player['id']));
+                    ?>
+                    <table class="table is-striped is-bordered is-hoverable is-fullwidth">
+                        <thead>
+                            <th>Played on</th>
+                            <th>Home Team</th>
+                            <th>Away Team</th>
+                            <th>Final Result</th>
+                            <th>More Details</th>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while($match = pg_fetch_assoc($result)):
+                            ?>
+                                <tr>
+                                    <td><?php echo date("d/m/Y", strtotime($match["played_on"])); ?></td>
+                                    <td><?php echo $match["hometeam_longname"]; ?></td>
+                                    <td><?php echo $match["awayteam_longname"]; ?></td>
+                                    <td><?php echo $match["hometeam_goals"], "-", $match["awayteam_goals"]; ?></td>
+                                    <td>
+                                        <a class="button is-small is-fullwidth" href="<?php echo sprintf("%s/view/match.php?id=%d", PAGES, $match["id"]);?>">View match</a>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
             <?php
                 endif;
             ?>
